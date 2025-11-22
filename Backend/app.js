b80b3a1852c49a0e8f703d0bd56ba2b08551fd5c
Rijwan Husain <husainrijwan2001@gmail.com>
2025-11-22 17:29:08 +0530

    Update CORS origin and adjust backend URL in environment file

--- Backend/app.js
+++ Backend/app.js
@@ -19,7 +19,7 @@ connectDB();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: ["https://starwar-app.vercel.app", "http://localhost:5173"],
+    origin: ["https://www.rijwan.me", "http://localhost:5173"],
     credentials: true,
 }));
 
